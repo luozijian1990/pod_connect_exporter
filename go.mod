ce20fc0ac78c583219ea5e42b5e0278032cdd290
luozijian <luozijian@banggood.com>
2025-05-30 10:18:30 +0800

    redo and fix bug

--- go.mod
+++ go.mod
@@ -1,31 +1,68 @@
 module pod_connect_exporter
 
-go 1.17
+go 1.21
+
+toolchain go1.22.2
 
 require (
-	github.com/florianl/go-conntrack v0.4.0
-	github.com/prometheus/client_golang v1.17.0
+	github.com/containerd/containerd v1.7.27
+	github.com/prometheus/client_golang v1.16.0
 )
 
 require (
-	github.com/BurntSushi/toml v0.4.1 // indirect
+	github.com/AdaLogics/go-fuzz-headers v0.0.0-20230811130428-ced1acdcaa24 // indirect
+	github.com/AdamKorcz/go-118-fuzz-build v0.0.0-20230306123547-8075edf89bb0 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/Microsoft/hcsshim v0.11.7 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
-	github.com/josharian/native v0.0.0-20200817173448-b6b71def0850 // indirect
+	github.com/containerd/cgroups v1.1.0 // indirect
+	github.com/containerd/containerd/api v1.8.0 // indirect
+	github.com/containerd/continuity v0.4.4 // indirect
+	github.com/containerd/errdefs v0.3.0 // indirect
+	github.com/containerd/fifo v1.1.0 // indirect
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/containerd/platforms v0.2.1 // indirect
+	github.com/containerd/ttrpc v1.2.7 // indirect
+	github.com/containerd/typeurl/v2 v2.1.1 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/go-events v0.0.0-20190806004212-e31b211e4f1c // indirect
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
-	github.com/mdlayher/netlink v1.5.0 // indirect
-	github.com/mdlayher/socket v0.1.0 // indirect
-	github.com/prometheus/client_model v0.4.1-0.20230718164431-9a2bf3000d16 // indirect
-	github.com/prometheus/common v0.44.0 // indirect
+	github.com/moby/locker v1.0.1 // indirect
+	github.com/moby/sys/mountinfo v0.6.2 // indirect
+	github.com/moby/sys/sequential v0.5.0 // indirect
+	github.com/moby/sys/signal v0.7.0 // indirect
+	github.com/moby/sys/user v0.3.0 // indirect
+	github.com/moby/sys/userns v0.1.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/opencontainers/runtime-spec v1.1.0 // indirect
+	github.com/opencontainers/selinux v1.11.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.42.0 // indirect
 	github.com/prometheus/procfs v0.11.1 // indirect
-	golang.org/x/mod v0.5.1 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/tools v0.1.7 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
-	honnef.co/go/tools v0.2.2 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.45.0 // indirect
+	go.opentelemetry.io/otel v1.21.0 // indirect
+	go.opentelemetry.io/otel/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/trace v1.21.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto v0.0.0-20231211222908-989df2bf70f3 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240401170217-c3f982113cda // indirect
+	google.golang.org/grpc v1.59.0 // indirect
+	google.golang.org/protobuf v1.35.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,217 +1,137 @@
 package main
 
 import (
+	"bufio"
 	"context"
-	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
-	ct "github.com/florianl/go-conntrack"
-	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
-	"net/url"
 	"os"
 	"os/signal"
+	"path/filepath"
+	"sort"
 	"strconv"
-	"sync"
+	"strings"
 	"syscall"
 	"time"
+
+	"github.com/containerd/containerd"
+	"github.com/containerd/containerd/namespaces"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
 
-type PodInfo struct {
-	Status string `json:"status"`
-	Data   Data   `json:"data"`
+var listenPort string
+
+type ContainerInfo struct {
+	PID       uint32
+	PodName   string
+	Namespace string
+	Container string
 }
 
-type Metric struct {
-	Namespace string `json:"namespace"`
-	Pod       string `json:"pod"`
+type TCPConnection struct {
+	Protocol   string
+	LocalIP    string
+	LocalPort  int
+	RemoteIP   string
+	RemotePort int
+	Status     string
 }
 
-type Result struct {
-	Metric Metric        `json:"metric"`
-	Value  []interface{} `json:"value"`
+type ConnectionStat struct {
+	RemoteIP   string
+	RemotePort int
+	Status     string
+	Count      int
 }
 
-type Data struct {
-	ResultType string   `json:"resultType"`
-	Result     []Result `json:"result"`
+type ConnectionKey struct {
+	RemoteIP   string
+	RemotePort int
+	Status     string
 }
 
 type ExporterMetrics struct {
-	podConnectStatus *prometheus.Desc
+	connectInfo *prometheus.Desc
 }
 
-var (
-	tcpStats = map[uint8]string{
-		1: "SYN_SENT",
-		3: "ESTABLISHED",
-		4: "FIN_WAIT",
-		5: "CLOSE_WAIT",
-		6: "LAST_ACK",
-		7: "TIME_WAIT",
-		8: "CLOSE",
-	}
-	podCIDR        string
-	prometheusAddr string
-	listenPort     string
-)
-
 func (collector *ExporterMetrics) Describe(ch chan<- *prometheus.Desc) {
-	ch <- collector.podConnectStatus
+	ch <- collector.connectInfo
 }
 
 func (collector *ExporterMetrics) Collect(ch chan<- prometheus.Metric) {
-	collector.getPodConnects(ch)
+	collector.collectConnectMetrics(ch)
 }
 
-func (collector *ExporterMetrics) getPodConnects(ch chan<- prometheus.Metric) {
-	nfct, err := ct.Open(&ct.Config{})
-	if err != nil {
-		log.Println("Could not create nfct:", err)
-		return
+func (collector *ExporterMetrics) collectConnectMetrics(ch chan<- prometheus.Metric) {
+	contains := listContains()
+
+	for _, container := range contains {
+		connections, err := getTCPConnections(int(container.PID))
+		if err != nil {
+			continue
+		}
+		stats := aggregateConnections(connections)
+
+		for _, stat := range stats {
+
+			ch <- prometheus.MustNewConstMetric(
+				collector.connectInfo,
+				prometheus.GaugeValue,
+				float64(stat.Count),
+				stat.RemoteIP,
+				strconv.Itoa(stat.RemotePort),
+				stat.Status,
+				container.PodName,
+				container.Namespace,
+				container.Container)
+		}
 	}
-	defer nfct.Close()
-	sessions, err := nfct.Dump(ct.Conntrack, ct.IPv4)
-	if err != nil {
-		log.Println("Could not dump sessions:", err)
-		return
-	}
-
-	var wg sync.WaitGroup
-
-	for _, session := range sessions {
-		wg.Add(1)
-
-		go func(session ct.Con) {
-			defer wg.Done()
-			if session.ProtoInfo == nil {
-				return
-			}
-
-			srcIP := session.Origin.Src.String()
-			srcPort := *session.Origin.Proto.SrcPort
-			dstIP := session.Origin.Dst.String()
-			dstPort := *session.Origin.Proto.DstPort
-
-			_, ipNet, _ := net.ParseCIDR(podCIDR)
-			ip := net.ParseIP(srcIP)
-
-			if ipNet.Contains(ip) {
-
-				queryStr := fmt.Sprintf(`kube_pod_info{pod_ip="%s"}`, srcIP)
-
-				info, err := getPodInfo(queryStr)
-
-				if err != nil {
-					return
-				}
-
-				if len(info.Data.Result) == 0 {
-					return
-				}
-
-				podName := info.Data.Result[0].Metric.Pod
-
-				podNameSpace := info.Data.Result[0].Metric.Namespace
-
-				ch <- prometheus.MustNewConstMetric(
-					collector.podConnectStatus,
-					prometheus.GaugeValue,
-					float64(1),
-					srcIP,
-					strconv.Itoa(int(srcPort)),
-					dstIP,
-					strconv.Itoa(int(dstPort)),
-					tcpStats[*session.ProtoInfo.TCP.State],
-					podName,
-					podNameSpace,
-				)
-			}
-		}(session)
-	}
-
-	wg.Wait()
 }
 
-func getPodInfo(query string) (PodInfo, error) {
-
-	prometheusUrl := fmt.Sprintf("http://%s/api/v1/query?query=", prometheusAddr)
-
-	requestUrl := fmt.Sprintf("%s%s", prometheusUrl, url.QueryEscape(query))
-
-	method := "GET"
-	client := &http.Client{}
-	req, err := http.NewRequest(method, requestUrl, nil)
-
-	if err != nil {
-		log.Println(err)
-		return PodInfo{}, err
-	}
-
-	res, err := client.Do(req)
-
-	if err != nil {
-		log.Println(err)
-		return PodInfo{}, err
-	}
-
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Println(err)
-		return PodInfo{}, err
-	}
-
-	var info PodInfo
-
-	err = json.Unmarshal(body, &info)
-	if err != nil {
-		log.Println(err)
-		return PodInfo{}, err
-	}
-
-	return info, nil
-}
-
-func podConnectMetrics() *prometheus.Desc {
+func connDesc() *prometheus.Desc {
 	return prometheus.NewDesc(
-		"pod_connect",
-		"pod connect info",
-		[]string{"src_ip", "src_port", "dst_ip", "dst_port", "stats", "pod_name", "pod_namespace"},
-		nil,
-	)
+		"pod_connect_info",
+		"Status of pod network connections (established, listening, etc.)",
+		[]string{"remote_addr", "remote_port", "status", "pod_name", "pod_namespace", "container"}, // 标签
+		nil)
 }
 
 func newMetricsCollector() *ExporterMetrics {
 	return &ExporterMetrics{
-		podConnectStatus: podConnectMetrics(),
+		connectInfo: connDesc(),
 	}
 }
 
+// 健康检查接口
+func health(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("health"))
+}
+
 func main() {
-	flag.StringVar(&podCIDR, "pod-cidr", "10.100.0.0/16", "pod cidr")
-	flag.StringVar(&prometheusAddr, "prometheus-addr", "10.101.20.9:9090", "prometheus addr")
+
 	flag.StringVar(&listenPort, "port", "28880", "exporter listen port")
 	flag.Parse()
 
 	allMetrics := newMetricsCollector()
 	prometheus.MustRegister(allMetrics)
 	http.Handle("/metrics", promhttp.Handler())
+	http.HandleFunc("/healthz", health)
 
 	server := &http.Server{Addr: fmt.Sprintf(":%s", listenPort)}
 
 	log.Printf("Starting server on port %s\n", listenPort)
 
 	go func() {
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 
 			log.Printf("ListenAndServe(): %s\n\n", err)
 			panic(err)
+
 		}
 	}()
 
@@ -219,7 +139,8 @@ func main() {
 	signal.Notify(sigChan, syscall.SIGTERM, os.Interrupt, syscall.SIGKILL)
 
 	// 阻塞,并等待signal信号
-	<-sigChan
+	sig := <-sigChan
+	log.Println(sig)
 	log.Println("SIGTERM received, shutting down gracefully...")
 
 	timeout, cancel := context.WithTimeout(context.Background(), 60*time.Second)
@@ -232,4 +153,207 @@ func main() {
 		log.Println("server Close Successful")
 	}
 
+}
+
+func listContains() []ContainerInfo {
+	client, err := containerd.New("/run/containerd/containerd.sock")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	// 设置上下文中的命名空间，默认是 "default"
+	ctx := namespaces.WithNamespace(context.Background(), "k8s.io")
+
+	var containersInfos []ContainerInfo
+
+	// 列出所有容器
+	containers, err := client.Containers(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//打印全部容器pid与labels
+	for _, container := range containers {
+		task, err := container.Task(ctx, nil)
+		if err != nil {
+			continue
+		}
+
+		labels, err := container.Labels(ctx)
+		if err != nil {
+			fmt.Printf("  Error getting labels: %v\n", err)
+			continue
+		}
+
+		s, b := labels["io.cri-containerd.kind"]
+
+		if b && s == "container" {
+			containersInfos = append(containersInfos, ContainerInfo{
+				PID:       task.Pid(),
+				PodName:   labels["io.kubernetes.pod.name"],
+				Namespace: labels["io.kubernetes.pod.namespace"],
+				Container: labels["io.kubernetes.container.name"],
+			})
+		}
+	}
+
+	return containersInfos
+}
+
+func hexToIP(hexIP string) string {
+	// 处理小端序的IPv4地址
+	if len(hexIP) == 8 {
+		var ipParts [4]int64
+		for i := 0; i < 4; i++ {
+			// 每两个字符一组，并反转顺序
+			pos := 6 - 2*i
+			ipPart, _ := strconv.ParseInt(hexIP[pos:pos+2], 16, 64)
+			ipParts[i] = ipPart
+		}
+		return fmt.Sprintf("%d.%d.%d.%d", ipParts[0], ipParts[1], ipParts[2], ipParts[3])
+	}
+	return "unknown"
+}
+
+// 十六进制字符串转为端口号
+func hexToPort(hexPort string) int {
+	port, _ := strconv.ParseInt(hexPort, 16, 32)
+	return int(port)
+}
+
+// 解析TCP连接状态
+func getTCPStatus(statusHex string) string {
+	statusMap := map[string]string{
+		"01": "ESTABLISHED",
+		"02": "SYN_SENT",
+		"03": "SYN_RECV",
+		"04": "FIN_WAIT1",
+		"05": "FIN_WAIT2",
+		"06": "TIME_WAIT",
+		"07": "CLOSE",
+		"08": "CLOSE_WAIT",
+		"09": "LAST_ACK",
+		"0A": "LISTEN",
+		"0B": "CLOSING",
+	}
+
+	if status, ok := statusMap[statusHex]; ok {
+		return status
+	}
+	return "UNKNOWN"
+}
+
+// 获取某个进程的TCP连接信息
+func getTCPConnections(pid int) ([]TCPConnection, error) {
+	var connections []TCPConnection
+
+	// 构建TCP文件路径
+	tcpFilePath := filepath.Join("/proc", strconv.Itoa(pid), "net", "tcp")
+
+	// 打开文件
+	file, err := os.Open(tcpFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("无法打开文件 %s: %v", tcpFilePath, err)
+	}
+	defer file.Close()
+
+	// 读取并解析文件内容
+	scanner := bufio.NewScanner(file)
+	lineCount := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// 跳过标题行
+		if lineCount == 0 {
+			lineCount++
+			continue
+		}
+
+		// 分割行内容
+		fields := strings.Fields(line)
+		if len(fields) < 4 {
+			continue
+		}
+
+		// 解析本地地址和端口
+		localAddrParts := strings.Split(fields[1], ":")
+		if len(localAddrParts) != 2 {
+			continue
+		}
+		localIP := hexToIP(localAddrParts[0])
+		localPort := hexToPort(localAddrParts[1])
+
+		// 解析远程地址和端口
+		remoteAddrParts := strings.Split(fields[2], ":")
+		if len(remoteAddrParts) != 2 {
+			continue
+		}
+		remoteIP := hexToIP(remoteAddrParts[0])
+		remotePort := hexToPort(remoteAddrParts[1])
+
+		// 跳过本地到本地的连接
+		if localIP == remoteIP && localIP == "127.0.0.1" {
+			continue
+		}
+
+		// 解析状态
+		status := getTCPStatus(fields[3])
+		// 创建连接信息
+		if status == "LISTEN" {
+			remotePort = localPort
+		}
+
+		conn := TCPConnection{
+			Protocol:   "TCP",
+			LocalIP:    localIP,
+			LocalPort:  localPort,
+			RemoteIP:   remoteIP,
+			RemotePort: remotePort,
+			Status:     status,
+		}
+
+		connections = append(connections, conn)
+		lineCount++
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("读取文件 %s 时出错: %v", tcpFilePath, err)
+	}
+
+	return connections, nil
+}
+
+// 聚合TCP连接信息
+func aggregateConnections(connections []TCPConnection) []ConnectionStat {
+	// 用map统计连接数
+	statsMap := make(map[ConnectionKey]int)
+
+	for _, conn := range connections {
+		key := ConnectionKey{
+			RemoteIP:   conn.RemoteIP,
+			RemotePort: conn.RemotePort,
+			Status:     conn.Status,
+		}
+		statsMap[key]++
+	}
+
+	// 转换为切片以便排序
+	var stats []ConnectionStat
+	for key, count := range statsMap {
+		stats = append(stats, ConnectionStat{
+			RemoteIP:   key.RemoteIP,
+			RemotePort: key.RemotePort,
+			Status:     key.Status,
+			Count:      count,
+		})
+	}
+
+	// 按计数降序排序
+	sort.Slice(stats, func(i, j int) bool {
+		return stats[i].Count > stats[j].Count
+	})
+
+	return stats
 }
